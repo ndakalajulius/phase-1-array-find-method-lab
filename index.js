04727f81579ef72a1727be40270b58d00f5362aa
ndakalajulius <julius.ndakala@student.moringaschool>
2022-08-21 09:45:25 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+const testrecord = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+
+    ];
+    function superbowlWin(array){
+
+        let win = array.find(({result}) => result === ("W"));
+        
+        if (win){
+        
+        return win.year;
+        
+        }
+        
+        else{
+        
+        return undefined
+        
+        }
+        
+        }
